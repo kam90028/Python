a93683cde1c5aeef94e12775f44f09e77664ac2a
Kevin Martin <kam90028@gmail.com>
2025-01-27 08:12:04 -0500

    wordlist.py for hangman

---
+++ hangman/wordlist.py
@@ -0,0 +1,3 @@
+words = ("elephant", "giraffe", "tiger", "koala", "dolphin", "penguin", "lemur", 
+         "bison", "cougar", "coyote", "buffalo", "zebra", "panther", "boar","monkey",
+         "apple", "banana", "orange", "watermelon", "grape", "pineapple", "aardvark","raccoon","skunk")
